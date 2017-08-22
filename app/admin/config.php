d46fd91ee352ec75df139f2cd35b1c4dfa1b7952
yxhsea <907811190@qq.com>
2017-08-22 22:08:19 +0800

    搭建初步系统框架

---
+++ app/admin/config.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * author      : Yxhsea.
+ * email       : Yxhsea@foxmail.com
+ * createTime  : 2017/8/22 21:43
+ * description :
+ */
+return [
+    // +----------------------------------------------------------------------
+    // | 应用设置
+    // +----------------------------------------------------------------------
+
+    /* 模板相关配置 静态资源文件路径配置*/
+    'TMPL_PARSE_STRING' => [
+        'admin'  =>  '/static/admin',
+        'layui'  =>  '/static/admin/layui'
+    ],
+];
---
+++ app/admin/controller/Index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * author      : Yxhsea.
+ * email       : Yxhsea@foxmail.com
+ * createTime  : 2017/8/22 21:00
+ * description :
+ */
+namespace app\admin\controller;
+
+class Index{
+    public function index(){
+        return 'admin';
+    }
+}
